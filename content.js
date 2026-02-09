a7f4460b835beac28bd587afe188408065b484fb
MaxencePeq <maxence.pequeno@icloud.com>
2026-02-09 15:27:42 +0100

    Feat : Récéption des messages et modification du style de la page

--- content.js
+++ content.js
@@ -1,3 +1,22 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    document.body.style.backgroundColor = request.color;
-});
+// Écoute les messages venant de popup.js
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+
+    if (message.type === 'links'){
+        // je récupère tous les liens avec le querySelectorAll
+        const liens = document.querySelectorAll('a');
+
+        // Je boucle dans les liens
+        liens.forEach(function (lien){
+
+            //dans le callback, je récupère la couleur
+            lien.style.color = message.color;
+        })
+    }
+
+    if(message.type === 'text'){
+        document.body.style.color = message.color;
+    }
+    if(message.type === 'background'){
+        document.body.style.backgroundColor = message.color;
+    }
+})
