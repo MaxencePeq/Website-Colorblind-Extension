a4dd2f00c1103c9e4e2da86ed6a2d5ddc26ec276
MaxencePeq <maxence.pequeno@icloud.com>
2026-02-09 14:50:25 +0100

    Feat : Modification du manifest.json + Création de la page HTML avec les color pickers et récupération des divs avec le JS

---
+++ popup.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function (){
+
+    // La je récupère avec les IDs les color pickers
+    const linkColorPicker = document.querySelector('.linkColor');
+    const textColorPicker = document.getElementById('textColor');
+    const bgColorPicker = document.getElementById('bgColor');
+
+
+})
