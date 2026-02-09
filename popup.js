8945d35202b365b7d01c63ad4c6fee8f6a3e07ac
MaxencePeq <maxence.pequeno@icloud.com>
2026-02-09 15:51:40 +0100

    feat : Ajout d'un bouton de reset + code pour revenir aux couleurs de bases

--- popup.js
+++ popup.js
@@ -2,6 +2,12 @@
 // !!! TOUS LES COMMENTAIRES SONT ÉCRIS A LA MAIN, POUR M'AIDER A LA COMPRÉHENSION !!!
 // ! AUCUNE IA N'A ÉTÉ UTILISÉ ICI.
 
+const couleursDeBase = {
+    links: '#0000ff',
+    text: '#000000',
+    background: '#ffffff'
+};
+
 
 document.addEventListener('DOMContentLoaded', function (){
 
@@ -10,6 +16,7 @@ document.addEventListener('DOMContentLoaded', function (){
     const linkColorPicker = document.querySelector('#linkColor');
     const textColorPicker = document.querySelector('#textColor');
     const bgColorPicker = document.querySelector('#bgColor');
+    const resetButton = document.querySelector('#backToBaseColor');
 
     // J'attends que le color pickers change de couleur
     linkColorPicker.addEventListener('change', function (){
@@ -29,6 +36,18 @@ document.addEventListener('DOMContentLoaded', function (){
     bgColorPicker.addEventListener('change', function (){
         const nouvelleCouleur = bgColorPicker.value;
         envoyerMessage('background', nouvelleCouleur)
+    })
+
+    // Quand on clique sur le bouton de reset
+    resetButton.addEventListener('click', function(){
+
+        linkColorPicker.value = couleursDeBase.links;
+        textColorPicker.value = couleursDeBase.text;
+        bgColorPicker.value = couleursDeBase.background;
+
+        envoyerMessage('links', couleursDeBase.links);
+        envoyerMessage('text', couleursDeBase.text);
+        envoyerMessage('background', couleursDeBase.background);
     })
 
 })
