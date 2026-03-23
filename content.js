c5527b8604c35754b86be38eec2f0f5debb30fce
MaxencePeq <maxence.pequeno@icloud.com>
2026-03-23 13:41:22 +0100

    Recractor : Suppression de l'ancien systeme de correction

--- content.js
+++ content.js
@@ -1,45 +1,31 @@
-// Écoute les messages venant de popup.js
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+chrome.runtime.onMessage.addListener(function (message) {
 
-    if (message.type === 'links'){
-        // je récupère tous les liens avec le querySelectorAll
+
+    /** Application des modifications personnalisées **/
+    if (message.type === 'links') {
         const liens = document.querySelectorAll('a');
-
-        // Je boucle dans les liens
-        liens.forEach(function (lien){
-
-            //dans le callback, je récupère la couleur
+        liens.forEach(function (lien) {
             lien.style.setProperty('color', message.color, 'important');
-        })
+        });
     }
 
-    if (message.type === 'text'){
-        // je cible tous les éléments de la page
+    if (message.type === 'text') {
         const elements = document.querySelectorAll('*');
-
-        elements.forEach(element => {
-            // et j'exclus les liens pour ne pas écraser leur couleur
-            if ((element.tagName !== 'A') || element.tagName !== 'a') {
+        elements.forEach(function (element) {
+            if (element.tagName !== 'A') {
                 element.style.setProperty('color', message.color, 'important');
             }
         });
     }
 
-
-    if(message.type === 'background'){
-        // J'applique sur body
-        document.body.style.backgroundColor = message.color;
-
-        // je trouve tous les éléments qui ont un background-color défini
-        const tousLesElements = document.querySelectorAll('*');
-
-        tousLesElements.forEach(element => {
+    if (message.type === 'background') {
+        const elements = document.querySelectorAll('*');
+        elements.forEach(function (element) {
             const bgColor = getComputedStyle(element).backgroundColor;
-
-            // Si mon élément a un background différent de transparent, j'applique la couleur
-            if(bgColor !== 'rgba(0, 0, 0, 0)' && bgColor !== 'transparent') {
+            if (bgColor !== 'rgba(0, 0, 0, 0)' && bgColor !== 'transparent') {
                 element.style.setProperty('background-color', message.color, 'important');
             }
         });
+        document.body.style.setProperty('background-color', message.color, 'important');
     }
-})
+});
