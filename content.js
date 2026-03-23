0b9d85124242fa0e96aefbe91feaa6f3815c3035
MaxencePeq <maxence.pequeno@icloud.com>
2026-03-23 13:53:48 +0100

    feat (js) : Instanciation des matrices de SVG de correction

--- content.js
+++ content.js
@@ -1,4 +1,50 @@
+// matrices de correction
+const matrices = {
+    protanopie: `
+        0.567, 0.433, 0,     0, 0
+        0.558, 0.442, 0,     0, 0
+        0,     0.242, 0.758, 0, 0
+        0,     0,     0,     1, 0
+    `,
+    deuteranopie: `
+        0.625, 0.375, 0,   0, 0
+        0.7,   0.3,   0,   0, 0
+        0,     0.3,   0.7, 0, 0
+        0,     0,     0,   1, 0
+    `,
+    tritanopie: `
+        0.95, 0.05,  0,     0, 0
+        0,    0.433, 0.567, 0, 0
+        0,    0.475, 0.525, 0, 0
+        0,    0,     0,     1, 0
+    `
+};
+
 chrome.runtime.onMessage.addListener(function (message) {
+
+    function appliquerFiltre(profil) {
+
+        // On crée un SVG invisible avec le filtre dedans
+        const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+        svg.id = 'daltonisme-svg-filter';
+        svg.style = 'position:absolute; width:0; height:0; overflow:hidden;';
+        svg.innerHTML = `
+        <defs>
+        <!-- filter sert à -->
+            <filter id="daltonisme-filter">
+                <feColorMatrix type="matrix" values="${matrices[profil]}"/>
+            </filter>
+        </defs>
+    `;
+
+        document.body.appendChild(svg);
+
+        //J'applique le filtre sur toute la page
+        document.documentElement.style.filter = 'url(#daltonisme-filter)';
+    }
+
+
+
 
 
     /** Application des modifications personnalisées **/
